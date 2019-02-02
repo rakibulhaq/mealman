7905e5d82fabe707e811378df681a39218b79c84
Rakibul Haq <rakib@funush.com>
2019-02-03 03:25:08 +0600

    initial project setup done

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,67 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header>
+        <h1>Meal Manager v 1.0.0</h1>
+      </el-header>
+      <el-row :gutter="20">
+        <el-col :span="4" class="el-col-left">
+          <el-menu default-active="1">
+            <el-menu-item index="1">
+              <span slot="title">Dashboard</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <span slot="title">Menu</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <span slot="title">Bills</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <span slot="title">Admin</span>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+        <el-col :span="20" class="grid-content">
+          <router-view></router-view>
+        </el-col>
+      </el-row>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.el-header {
+  height: 60px;
+  background-color: aliceblue;
+}
+.el-row {
+  width: 100%;
+}
+.el-col-left {
+  text-align: left;
+}
+.grid-content {
+  background-color: aliceblue;
+  border-radius: 4px;
+  min-height: 360px;
 }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Menu, MenuItem, Submenu, MenuItemGroup, Container, Header, Aside, Main, Col, Row } from 'element-ui'
+import 'element-ui/lib/theme-chalk/reset.css'
 import lang from 'element-ui/lib/locale/lang/en'
 import locale from 'element-ui/lib/locale'
 
 locale.use(lang)
 
 Vue.use(Button)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Col)
+Vue.use(Row)
